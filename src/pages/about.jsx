263123249d114aad9869228efca3f41fd525e005
MBALVIN <mzenteno@asap-consulting.net>
2026-04-25 17:57:06 -0300

    comentario de prueba

--- src/pages/about.jsx
+++ src/pages/about.jsx
@@ -1,5 +1,6 @@
 export default function About() {
   return <div className="flex flex-col gap-4">
     <h1 className="text-2xl font-bold">Acerca de AI Code Review Platform</h1>
-    </div>
+    </div> 
+    //modifico nombre
 }
