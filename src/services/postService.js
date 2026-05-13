908e2d2b4ec8074aa140b2f38c842c5da07e6efc
MBALVIN <mzenteno@asap-consulting.net>
2026-05-13 10:29:35 -0300

    direcciono a endpoint profile

--- src/services/postService.js
+++ src/services/postService.js
@@ -2,7 +2,7 @@ import { apiClient } from "../api/client";
 
 export const postService = {
   getPost: async () => {
-    const { data } = await apiClient.get("/me");
+    const { data } = await apiClient.get("/auth/me");
     return data;
   },
 };
